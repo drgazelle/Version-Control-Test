9a7c5450866d9778cc0bffb2ead69f02f4c8b8ee
1527008 <64329293+drgazelle@users.noreply.github.com>
2022-09-14 08:28:34 -0400

    V0.1

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * HelloWorld class returns phrase
+ * as string
+ */
+public class HelloWorld {
+    public static String getPhrase() {
+        return "hello world!";
+    }
+}
---
+++ src/VCSDriver.java
@@ -0,0 +1,9 @@
+/**
+ * VCSDriver class is the main method that calls upon
+ * a subclass
+ */
+public class VCSDriver {
+    public static void main(String[] args) {
+        System.out.println(HelloWorld.getPhrase());
+    }
+}
